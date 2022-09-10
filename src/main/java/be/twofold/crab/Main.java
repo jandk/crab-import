1df8432295d0a1854d0b9f96040fc654e2a733b1
Jan De Kock <jan@twofold.be>
2022-09-10 12:58:11 +0200

    Rename

--- src/main/java/be/twofold/crab/Main.java
+++ src/main/java/be/twofold/crab/Main.java
@@ -6,15 +6,18 @@ import java.sql.*;
 import java.util.*;
 import java.util.stream.*;
 
-public class CrabImport {
+public final class Main {
+
     public static void main(String[] args) throws IOException, SQLException {
         if (args.length != 1) {
             System.out.println("Usage: CrabImport <path to crab data>");
             System.exit(1);
         }
 
-        List<Path> filePaths = scanFiles(Paths.get(args[0]));
+        importFiles(scanFiles(Paths.get(args[0])));
+    }
 
+    private static void importFiles(List<Path> filePaths) throws SQLException, IOException {
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/crab?reWriteBatchedInserts=true", "crab", "crab")) {
             connection.setAutoCommit(false);
             Importer importer = new Importer(connection);
